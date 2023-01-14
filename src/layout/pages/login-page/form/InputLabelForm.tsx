c7d20c30899a73db4751f83c65269ce5f862fb31
KirillDanProg <kirill2898v@gmail.com>
2023-01-14 11:57:32 +0300

    feat: date input restrictions added

--- src/layout/pages/login-page/form/InputLabelForm.tsx
+++ src/layout/pages/login-page/form/InputLabelForm.tsx
@@ -1,26 +1,31 @@
-import React, {ChangeEvent, FC} from 'react';
+import React, {ChangeEvent, forwardRef, MutableRefObject, ReactElement} from 'react';
 import commonS from "common/styles/CommonStyles.module.scss";
 
-export const InputLabelForm: FC<PropsType> = ({error, touched, htmlFor, ...restProps}) => {
+export const InputLabelForm = forwardRef<null, PropsType>(({error, touched, htmlFor, ...restProps}, ref) => {
     return (
         <div className={commonS.formItemContainer}>
             <label htmlFor={htmlFor}>{htmlFor}</label>
             <input
+                ref={ref}
                 {...restProps}
                 className={commonS.input}
                 autoComplete={"off"}
             />
             {error && touched && error}
+            {restProps.icon}
         </div>
     );
-};
+});
 
 type PropsType = {
     id: string
     type: string
-    onChange: (e: string | ChangeEvent<any>) => void
     value: string | number
     htmlFor: string
     error: string | undefined
     touched: boolean | undefined
+    onChange: (e: any | ChangeEvent<any>) => void
+    icon?: ReactElement<any, any>
+    ref?: MutableRefObject<null>
+    min?: string
 }
